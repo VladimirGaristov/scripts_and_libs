ff11149ec145a0be1eaf8d8882b705a746b1dea1
Владо <vl.garistov@gmail.com>
2021-12-24 20:37:52 +0200

    removed unnecesary crap

--- autodj.sh
+++ autodj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# AutoDJ v2.0 - download a song and add it to a queue in Mixxx
+# AutoDJ v2.1 - download a song and add it to a queue in Mixxx
 # Copyright (C) 2021 Vladimir Garistov <vl.garistov@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
@@ -14,32 +14,17 @@
 #
 
 # This script reads a song name from standard input, searches for it on YouTube, downloads only the audio from the
-# first result and adds it to the AutoDJ queue in Mixxx. It relies on Mixxx being configured to automatically
-# add as many songs to the AutoDJ queue as it finds in the Music library. This is achieved by setting the
-# following in ~/.mixxx/mixxx.cfg :
-# [Auto DJ]
-# EnableRandomQueue 1
-# EnableRandomQueueBuff 1
-# RandomQueueMinimumAllowed 1000
-# RandomQueueMinimumAllowedBuff 1000
-# Requeue 0
+# first result and adds it to the AutoDJ queue in Mixxx.
 #
-# The script also relies on hard-coded coordinates from which it reads the next song in the Mixxx queue. It does
-# so by taking a screenshot of the Mixxx GUI and passing it through OCR. You must configure these coordinates
+# The script relies on hard-coded coordinates on which it clicks. You must configure these coordinates
 # based on your monitor. Obviously changing the window size of Mixxx in any way will interfere with the correct
 # operation of this script. It is recomended to leave Mixxx in fullscreen on the primary display.
 #
 # It is recommended to clear the AutoDJ queue after starting the script. Add at least two songs before starting
-# the AutoDJ function in Mixxx to avoid playing the first song twice. The first song always gets requeued but
-# it only plays once (unless AutoDJ was started in Mixxx with only one queued song).
-# Songs are added to the AutoDJ queue in Mixxx by placing them in the music library, rescanning the library
-# and relying on Mixxx to add the automatically. Mixxx adds random songs from the library to the queue whenever
-# a song finishes playing. An unfortunate side effect of this is that if multiple songs are added while one
-# song is playing the newly added songs are shuffled before being appended to the end of the queue.
+# the AutoDJ function in Mixxx to avoid playing the first song twice. 
 #
 # Known issues:
 # -very long song names that only differ in the last parts of their names can be mistaken for each other
-#
 #
 # Version 1.0 uses VLC instead of Mixxx and is simpler, more reliable and more lightweight. However, VLC does not
 # support crossfading between songs and doesn't have the AutoDJ functionality that Mixxx has.
@@ -47,116 +32,22 @@
 # Set the directory where AutoDJ will download all songs. It must be the Mixxx library or preferably a folder in it.
 # No trailing slash!
 download_location='/home/cartogan/Music/autodj'
+buffer_dir='/var/tmp/autodj/buffer'
 # Time in ms to wait after reloading Mixxx music library
 mixxx_lib_reload_time=500
-# Time to wait between two clicks to avoid them being detected as a doubleckick
+# Time in ms to wait between two clicks to avoid them being detected as a doubleckick
 click_delay_time=500
-
-# Helper function for trimming whitespace
-trim()
-{
-    local var="$*"
-    # remove leading whitespace characters
-    var="${var#"${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
-    var="${var%"${var##*[![:space:]]}"}"
-    printf '%s' "$var"
-}
-
-# Runs in a child process, periodically checks if the next-in-queue song has changed.
-# If it has, renames the audio file so Mixxx won't automatically add the same song again.
-# The file is renamed and not deleted to prevent another process from overwriting the data on disk
-# while Mixxx is playing the deleted but still open file.
-song_file_remover()
-{
-	local mixxx_pid=$1
-	sleep 10
-
-	# Wait for a song to start playing
-	mixxx_titlebar=$(xprop -id "$(xdo id -p $mixxx_pid)" _NET_WM_NAME | grep Mixxx | tr -d '"')
-	mixxx_titlebar=${mixxx_titlebar//'_NET_WM_NAME(UTF8_STRING) = '/''}
-	mixxx_wid=$(xdo id -a "$mixxx_titlebar")
-	current_song_file=${mixxx_titlebar//' | Mixxx'/'.m4a'}
-	# If no song is currently playing then the window title is just 'Mixxx'. Otherwise it contains the song name.
-	while [[ $current_song_file == 'Mixxx' ]]
-	do
-		sleep 1
-		mixxx_titlebar=$(xprop -id "$(xdo id -p $mixxx_pid)" _NET_WM_NAME | grep Mixxx | tr -d '"')
-		mixxx_titlebar=${mixxx_titlebar//'_NET_WM_NAME(UTF8_STRING) = '/''}
-		current_song_file=${mixxx_titlebar//' | Mixxx'/'.m4a'}
-	done
-	# Rename the currently playing file to keep the data from being overwritten but prevent Mixxx from
-	# adding it back to the queue
-	if [ -f "$download_location/$current_song_file" ]
-	then
-		rm "$download_location/.KUR" >/dev/null 2>&1
-		mv "$download_location/$current_song_file" "$download_location/.KUR"
-	fi
-	alt_name=true
-	prev_queued=''
-
-	# Every time the queued song changes rename it
-	while [ true ]
-	do
-		# Take a screenshot of the Mixxx window
-		import -quiet -window "$mixxx_wid" /var/tmp/autodj/queue.png
-		# Crop out the name of the next song
-		# Below are the window size dependent coordinates. Make sure you adjust them to match your setup!
-		convert /var/tmp/autodj/queue.png -crop 375x20+629+629 /var/tmp/autodj/queue_cropped.png
-		convert /var/tmp/autodj/queue_cropped.png -channel RGB -negate /var/tmp/autodj/queue_cropped_inverted.png
-		convert /var/tmp/autodj/queue_cropped_inverted.png +repage -crop 144x20+0+0 /var/tmp/autodj/artist.png
-		convert /var/tmp/autodj/queue_cropped_inverted.png +repage -crop 231x20+145+0 /var/tmp/autodj/title.png
-		# Read it using OCR
-		artist=$(tesseract /var/tmp/autodj/artist.png - 2>/dev/null)
-		artist=$(trim "$artist")
-		title=$(tesseract /var/tmp/autodj/title.png - 2>/dev/null)
-		title=$(trim "$title")
-		artist=${artist//'...'/''}
-		title=${title//'...'/''}
-		queued=$(ls -1 $download_location | grep "$artist" | grep "$title")
-		echo $queued
-
-		if [[ $queued != $prev_queued ]]
-		then
-			# Wait for the transition between songs to finish
-			sleep 12
-			# Alternate between two bogus placeholder filenames
-			if [ "$alt_name" = true ]
-			then
-					if [ -f "$download_location/$queued" ]
-					then
-						rm "$download_location/.KURCHE" >/dev/null 2>&1
-						mv "$download_location/$queued" "$download_location/.KURCHE"
-					fi
-					alt_name=false
-			else
-					if [ -f "$download_location/$queued" ]
-					then
-						rm "$download_location/.KUR" >/dev/null 2>&1
-						mv "$download_location/$queued" "$download_location/.KUR"
-					fi
-					alt_name=true
-			fi
-		fi
-
-		prev_queued=$queued
-		sleep 5
-	done
-}
-
 
 # No, neither xdo nor xdotool works reliably for getting the PID of the parent terminal emulator
 autodj_pid=$(ps -o ppid= -p $(ps -o ppid= -p $$) | awk '{$1=$1;print}')
 
-rm "$download_location"/* >/dev/null 2>&1
 mkdir -p /var/tmp/autodj
+mkdir -p /var/tmp/autodj/buffer
+rm /var/tmp/autodj/buffer/* >/dev/null 2>&1
 
 mixxx >/dev/null 2>&1 &
 mixxx_pid=$!
 sleep 3
-
-# Failed attempt
-#song_file_remover $mixxx_pid &
 
 # Unicode symbols in video titles are escaped as bytes which are represented with \x notation
 # For example the cyrilic letter "Щ" (sht) becomes "\xd0\xa9" - 8 bytes in total
@@ -201,9 +92,8 @@ do
 	curl "https://www.youtube.com/results?search_query=$song" > /var/tmp/autodj/yt_results.html
 
 	sed -i -e 's/\"/\n/g' /var/tmp/autodj/yt_results.html
-	sed '1,/Изпробване/d' /var/tmp/autodj/yt_results.html > /var/tmp/autodj/yt_results.txt
 	# Obtain the URL of the first result
-	url="https://www.youtube.com$(cat /var/tmp/autodj/yt_results.txt | grep watch | head -n 1)"
+	url="https://www.youtube.com$(cat /var/tmp/autodj/yt_results.html | grep '/watch?v=' | head -n 1)"
 	echo $url
 
 	if [ "$url" = 'https:://www.youtube.com' ]
@@ -212,11 +102,11 @@ do
 	fi
 
 	# Download only the audio of this video
-	yt-dlp -f "m4a" -o "$download_location/tmp/%(title).$(echo $max_filename_lenght)s.%(ext)s" --no-playlist $url
+	yt-dlp -f "m4a" -o "$buffer_dir/%(title).$(echo $max_filename_lenght)s.%(ext)s" --no-playlist $url
 
-	# The new song will be the latest file in the directory
-	filename="$(ls -t $download_location/tmp | head -n1)"
-	if [ ! -f "$download_location/tmp/$filename" ]
+	# The new song will be the only file in the directory
+	filename="$(ls $buffer_dir)"
+	if [ ! -f "$buffer_dir/$filename" ]
 	then
 		continue
 	fi
@@ -226,9 +116,9 @@ do
 	# it cannot be determined which file is the latest
 	if [ $existing_copies -gt 0 ]
 	then
-		mv "$download_location/tmp/$filename" "$download_location/${filename//.m4a/''}-${existing_copies}.m4a"
+		mv "$buffer_dir/$filename" "$download_location/${filename//.m4a/''}-${existing_copies}.m4a"
 	else
-		mv "$download_location/tmp/$filename" "$download_location/$filename"
+		mv "$buffer_dir/$filename" "$download_location/$filename"
 	fi
 
 	# Add the song to the current playlist

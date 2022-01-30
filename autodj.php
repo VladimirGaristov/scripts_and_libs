26f7713a38d0062b93b777e037b76a260ade7a53
Vladimir Garistov <vl.garistov@gmail.com>
2022-01-30 21:14:59 +0200

    added special song name for deleting the first song

--- autodj.php
+++ autodj.php
@@ -2,12 +2,26 @@
 <html>
 	<body>
 		<?php
+			function strip_first_line($text)
+			{        
+				return substr($text, strpos($text, "\n") + 1);
+			}
+
 			if (isset($_POST['song_name']))
 			{
-				$fp = fopen("songlist.txt", "a") or die("Unable to open file");
 				$song_name = $_POST['song_name']."\n";
-				fwrite($fp, $song_name);
-				fclose($fp);
+				if $song_name == "72DkILZfdCutpUxMc50846ChapOhAaJrXaNGanaqdQb3hxhLDZHq48JgBRre3eSqrsu2fEY09oMgPmnnjN1l5deqzVrjrLdXwyGSXL7FX07sy8jXEunqhzruGaZNBtev"
+				{
+					$songlist = file_get_contents('songlist.txt');
+					$songlist = strip_first_line($songlist);
+					file_put_contents('songlist.txt', $songlist);
+				}
+				else
+				{
+					$fp = fopen("songlist.txt", "a") or die("Unable to open file");
+					fwrite($fp, $song_name);
+					fclose($fp);
+				}
 			}
 		?>
 
@@ -17,5 +31,9 @@
 		<br>
 		<input type="submit" value="Request">
 		</form>
+
+		<button onclick="window.location.href='https://garistov.idiotempire.com';">
+			Back
+		</button>
 	</body>
 </html>

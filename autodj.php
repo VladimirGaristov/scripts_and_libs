6f3a389923e2aeaf500550c71825dfabf661aed0
Vladimir Garistov <vl.garistov@gmail.com>
2022-01-30 20:20:46 +0200

    added php script

---
+++ autodj.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<?php
+			if (isset($_POST['song_name']))
+			{
+				$fp = fopen("requested.txt", "a") or die("Unable to open file");
+				$song_name = $_POST['song_name'];
+				fwrite($fp, $song_name);
+				fclose($fp);
+			}
+		?>
+
+		<form action="autodj.php" method="post">
+		Song title: 
+		<input type="text" name="song_name">
+		<br>
+		<input type="submit" value="Request">
+		</form>
+	</body>
+</html>
